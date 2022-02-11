68b2a1fcf2933a189e5c2f1125614a74bffa0e66
Anand.Narayan <arnarayan@gmail.com>
2022-02-10 17:52:46 -0700

    Initial Commit

---
+++ Module2Lesson16/Module2Lesson16App.swift
@@ -0,0 +1,17 @@
+//
+//  Module2Lesson16App.swift
+//  Module2Lesson16
+//
+//  Created by Anand Narayan on 2022-02-10.
+//
+
+import SwiftUI
+
+@main
+struct Module2Lesson16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
